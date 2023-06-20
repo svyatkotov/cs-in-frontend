c47cf536573e57fe7b9843452f338d471409f324
Svyatoslav <skd946@gmail.com>
2023-06-20 15:31:43 +0300

    Refactor task-4, task-5 in hw-4

--- src/4-regular-expressions/4-replace/replace.ts
+++ src/4-regular-expressions/4-replace/replace.ts
@@ -1,5 +1,5 @@
 export const myRegExp = /([a-z]{1,3}?)\1+/g;
 
-export function replaceVal(...args: any[]): string {
-    return args[1];
+export function replaceVal(_: string, group: string): string {
+    return group;
 }
--- src/4-regular-expressions/5-calc/calc.ts
+++ src/4-regular-expressions/5-calc/calc.ts
@@ -1,3 +1,3 @@
 export function calc(str: string): string {
-    return str.replace(/[(\d-][()\d\-+*/ ]+[)\d]/g, (...args) => eval(args[0]));
+    return str.replace(/[(\d-][()\d\-+*/ ]+[)\d]/g, str => eval(str));
 }
